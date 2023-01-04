d01a0c7d113be99680f1c93a679ab446ed08a6d9
Cavidismailzadeh <javidai@code.edu.az>
2023-01-04 18:32:39 +0400

    New

---
+++ ClassWork/ClassWork/Models/Eagle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWork.Models
+{
+    internal class Eagle : Bird
+    {
+        public string speed { get; set; }
+
+        public override void Sound()
+        {
+            Console.WriteLine("Eagle Sound");
+        }
+    }
+}
